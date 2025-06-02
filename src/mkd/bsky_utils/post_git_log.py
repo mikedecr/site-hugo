549383a21a94a55ffaa9c6f7aa7641258aa56f9c
mikedecr <mgdecrescenzo@gmail.com>
2025-06-01 19:17:26 -0500

    remove commit hash from bsky post text, nobody will care about that

--- src/mkd/bsky_utils/post_git_log.py
+++ src/mkd/bsky_utils/post_git_log.py
@@ -4,6 +4,9 @@ from bsky_bridge import post_text as bsky_post_text
 from .bsky_session import session as bsky_session
 from .git_log import GitLog
 
+from logging import getLogger
+logger = getLogger(__name__)
+
 
 def date_post_text(repo_name: str, gitlog: GitLog):
     log_data: list[dict[str, str]] = gitlog.messages()
@@ -15,15 +18,16 @@ def date_post_text(repo_name: str, gitlog: GitLog):
         ":"
     ])
     commits = [
-        f"- {commit['hash']}: {commit['message']}" for commit in log_data
+        f"- {commit['message']}" for commit in log_data
     ]
     return '\n'.join([header, *commits])
 
 
 def post_git_log(repo_name: str, gitlog: GitLog, dry_run=True):
     text: str = date_post_text(repo_name=repo_name, gitlog=gitlog)
-    rprint(text)
     if dry_run:
+        logger.info("as dry run:\n---\n%s", text)
         return
     else:
+        logger.info("Posting for real:\n%s", text)
         return bsky_post_text(session=bsky_session, text=text)
